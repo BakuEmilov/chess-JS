c2438e53457ff46574d5c651fc013e7084d3baed
BakuEmilov <emilovbaku530@gmail.com>
2023-06-05 15:14:17 +0600

    painted the chess board

--- logic.js
+++ logic.js
@@ -17,3 +17,25 @@ function insertImages() {
 }
 
 insertImages();
+
+//Раскрашивание доски
+function coloring () {
+  const color = document.querySelectorAll('.box')
+
+  color.forEach(color =>{
+    getId=color.id
+    arr = Array.from(getId)
+    arr.shift()
+    aside = eval(arr.pop())
+    aup = eval(arr.shift())
+    a = aside + aup
+
+    if (a % 2 == 0){
+      color.style.backgroundColor = 'rgb(232 235 239)'
+    }
+    if (a % 2 !== 0){
+      color.style.backgroundColor = 'rgb(125 135 150)'
+    }
+  })
+}
+coloring()
